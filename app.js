2f3b61555995bc7aeb67227642c6ae013ba93d28
shen5455 <1595552404@qq.com>
2016-09-14 14:21:59 +0800

    add pages

--- app.js
+++ app.js
@@ -5,61 +5,81 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var juicerExpressAdapter = require('juicer-express-adapter');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
-app.engine('.html', ejs.__express);
+
+
 app.set('view engine', 'html');
+// app.engine('.html', ejs.__express);
+app.engine('html', juicerExpressAdapter);
+
+
+// app.engine('.html', function(path, options, fn) {
+// 	fs.readfile(path, 'utf8', function(err, str) {
+// 		if (err) return fn(err);
+// 		str = juicer(str, options);
+// 		fn(null, str);
+// 	});
+// });
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
+
+// app.use('/', routes);
+// app.use('/users', users);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+// 	res.render('index', {
+// 		title: 'Express'
+// 	});
+// });
+// router.get('/home', function(req, res, next) {
+// 	res.render('home');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+	app.get('/home', function(req, res) {
+		res.render('home');
+	});
+	app.get('/test', function(req, res) {
+		res.render('example');
+	});
+
+	app.get('/person', function(req, res) {
+		res.render('person');
+	});
+	app.get('/question', function(req, res) {
+		res.render('question');
+	});
+}
